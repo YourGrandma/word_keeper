f207ffc5b9fbba2c7f04757d48687c76deae5c58
k.sychev <klimentii1995.02@bk.ru>
2021-05-23 23:29:04 +0300

    update "to" routes; add scoped style; add DragButtonIcon;

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
         <div id="nav">
 
-            <router-link to="/">Word Keeper</router-link>
-            <router-link to="/favorites">Starred words</router-link>
+            <router-link :to="{name: 'Home'}">Word Keeper</router-link>
+            <router-link :to="{name: 'Favorites'}">Starred words</router-link>
 
         </div>
 
@@ -16,44 +16,45 @@
 </template>
 
 <style>
-body {
 
-    background-color: #efefef;
-    margin:0;
-    height:100%
-}
+    body {
 
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    padding:12px;
-    margin: 0;
-    height:100%;
-}
+        background-color: #efefef;
+        margin:0;
+        height:100%
+    }
 
-#nav {
-    display: flex;
-    flex-shrink: 1;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding: 16px 32px;
-    background-color: #6ec0fa;
-    border-radius:3px;
-    margin-bottom:30px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding:12px;
+        margin: 0;
+        height:100%;
+    }
 
-#nav a {
+    #nav {
+        display: flex;
+        flex-shrink: 1;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding: 16px 32px;
+        background-color: #6ec0fa;
+        border-radius:3px;
+        margin-bottom:30px;
+    }
 
-    font-weight: bold;
-    color: white;
-}
+    #nav a {
 
-#nav a.router-link-exact-active {
-    color: white;
-}
+        font-weight: bold;
+        color: white;
+    }
+
+    #nav a.router-link-exact-active {
+        color: white;
+    }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,8 @@ export default {
 
 }
 </script>
-<style>
+
+<style scoped>
 
     .home {
 
